b56f1d5c265fe0dccbb68e8ed1b05c952e5ca83a
xakim <xakim@pop-os.localdomain>
2020-09-30 20:42:27 +0600

    Updates

---
+++ precache-manifest.14b4ad5ca1c8b37c9c4486bb26ac543d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3547e8028e7a570749c5c7c8da84742",
+    "url": "/ParliamentCalculator/index.html"
+  },
+  {
+    "revision": "c5212af072505c29d444",
+    "url": "/ParliamentCalculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b8ad399cb303b0663b8d",
+    "url": "/ParliamentCalculator/static/js/2.1451b32e.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/ParliamentCalculator/static/js/2.1451b32e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5212af072505c29d444",
+    "url": "/ParliamentCalculator/static/js/main.fc7cc19f.chunk.js"
+  },
+  {
+    "revision": "03de488d8d6c344b4c9b",
+    "url": "/ParliamentCalculator/static/js/runtime-main.da924a04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ParliamentCalculator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d6ae188912c1b4c4c29c4de7342d12f1",
+    "url": "/ParliamentCalculator/static/media/Бир Бол.d6ae1889.png"
+  },
+  {
+    "revision": "5f98b378fce0a6bd8d166083d5432ee3",
+    "url": "/ParliamentCalculator/static/media/Кыргызстан.5f98b378.png"
+  },
+  {
+    "revision": "b9d0c5d3dc30b0d0bce829e7b98c7807",
+    "url": "/ParliamentCalculator/static/media/Мекен Ынтымагы.b9d0c5d3.png"
+  },
+  {
+    "revision": "d975dd8f04f9be16f265c3983a306475",
+    "url": "/ParliamentCalculator/static/media/Политическая партия ветеранов войны в Афганистане и участников других боевых конфликтов.d975dd8f.png"
+  },
+  {
+    "revision": "3f7000b1ad13ad7b4a33f2cefb24a9f0",
+    "url": "/ParliamentCalculator/static/media/Чон казат.3f7000b1.png"
+  },
+  {
+    "revision": "e1489fd6f4537782821aecc61003e5a5",
+    "url": "/ParliamentCalculator/static/media/Ыйман Нуру.e1489fd6.png"
+  }
+]);
